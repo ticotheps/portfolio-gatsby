48d6781dd82a0c68ffd9133bcfd36e4966480d5a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-03 17:47:22 -0400

    Adds an <h1> and a <p> tag to index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+  </div>
 )
