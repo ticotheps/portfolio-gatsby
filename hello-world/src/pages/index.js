bba9eb9f675c0ff19834092cb617d889abcfc684
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-03 18:30:59 -0400

    Creates a <Header /> component

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
+    <Header />
     <p>What a world.</p>
     <img
       src="https://source.unsplash.com/random/400x200"
