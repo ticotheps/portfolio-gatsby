8876f048a5006861de04f278fe96511a98dd246a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-03 18:35:32 -0400

    Passes 'headerText' prop into <Header> component in both index.js and about.js page components

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Header />
+    <Header headerText="Hello Gatsby!" />
     <p>What a world.</p>
     <img
       src="https://source.unsplash.com/random/400x200"
