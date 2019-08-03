263dab365934438549430a4f5c2259484ae87c04
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-03 17:48:54 -0400

    Adds random photo to index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,5 +4,9 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
+    <img
+      src="https://source.unsplash.com/random/400x200"
+      alt="random option from Unsplash.com"
+    />
   </div>
 )
