d4a4ec9f5c056dc2cd523f70ae1e9daf618d5668
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-04 16:12:53 -0400

    Creates a production build to deploy to surge

--- hello-world/.cache/async-requires.js
+++ hello-world/.cache/async-requires.js
@@ -2,10 +2,9 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-about-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-index-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-404-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-contact-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */)
+  "component---src-pages-about-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-contact-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
