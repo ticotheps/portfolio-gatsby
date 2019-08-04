2833ea9ad7e135694acf741abb2f460b84db9a76
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-04 15:49:30 -0400

    Creates a 404.htm.js page to satisfy error in compiler

--- hello-world/.cache/sync-requires.js
+++ hello-world/.cache/sync-requires.js
@@ -7,6 +7,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/.cache/dev-404-page.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js"))),
+  "component---src-pages-404-htm-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/404.htm.js")))
 }
 
