50951eacfbd88d2d44330516e89685023e125f4d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-04 16:12:53 -0400

    Creates a production build to deploy to surge

--- hello-world/.cache/sync-requires.js
+++ hello-world/.cache/sync-requires.js
@@ -5,10 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/.cache/dev-404-page.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/404.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/contact.js")))
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js")))
 }
 
