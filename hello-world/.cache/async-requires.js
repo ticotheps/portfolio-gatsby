c2e7b9cddfa060bcd954cb11922db103d0bb79b0
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-04 15:45:18 -0400

    Deletes package.lock and reinstalls yarn

--- hello-world/.cache/async-requires.js
+++ hello-world/.cache/async-requires.js
@@ -3,7 +3,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-about-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */)
+  "component---src-pages-about-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-index-js": () => import("/Users/ticothep/my-projects/portfolio-gatsby/hello-world/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
