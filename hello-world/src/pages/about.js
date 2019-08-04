28d74fe73bd343d5c2abd48db3662104af7bc0ae
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-04 15:34:11 -0400

    Creates an example of how to pass props in react

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -4,6 +4,7 @@ import Header from "../components/header"
 export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby" />
+    <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </div>
 )
