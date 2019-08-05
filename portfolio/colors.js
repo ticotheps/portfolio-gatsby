759a6c2cf92a0aeb1b7f8093a9fbcb2c50afd84c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-05 19:58:50 -0400

    Changes colors in colors.js

--- portfolio/colors.js
+++ portfolio/colors.js
@@ -1,12 +1,15 @@
 module.exports = {
-  background: '#FFFFFF',
-  backgroundDark: '#f0e6f6',
+  background: '#FFFFFF' /* color of links at top of page; white */,
+  backgroundDark:
+    '#c4fffd' /* color of small middle triangle on lading page; light robin's egg blue */,
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary:
+    '#000dff' /* color of "Hello..." text and card links; dark royal blue */,
+  primaryLight: '#23bff7' /* color of card links on hover text; aqua */,
+  primaryDark: '#3257fa' /* background color of sticky nav; royal blue */,
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#05d1ff' /* color of upper, left-most triangle; twitter blue */,
+  secondaryLight:
+    '#21ffb9' /* color of middle stripe/triangle running through pate; pastel green */,
+  secondaryDark: '#2ad19c' /* color of section headings; dark pastel green */,
 };
