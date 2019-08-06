cb05e157437ae422d12dba90bf58a0f74cdddd5f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-06 15:44:21 -0400

    Updates Footer.js

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => (
           <FooterContainer>
             <Fade left>
               <TextFooter>
-                <span>{`${name} Portfolio - Powered by `}</span>
+                <span>{`${name}'s Portfolio - Powered by `}</span>
                 <Link href="https://www.gatsbyjs.org/">Gatsby</Link>
                 <span> and </span>
                 <Link href="https://www.contentful.com/" mr={1}>
