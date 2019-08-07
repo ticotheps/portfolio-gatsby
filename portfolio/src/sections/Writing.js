ed03cbaca9432f3e4f00dc461773a7f00b9f5b06
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-06 21:28:27 -0400

    Updates section headings

--- portfolio/src/sections/Writing.js
+++ portfolio/src/sections/Writing.js
@@ -182,7 +182,7 @@ const Writing = () => (
       return (
         isMediumUserDefined && (
           <Section.Container id="writing" Background={Background}>
-            <Section.Header name="Writing" icon="✍️" label="writing" />
+            <Section.Header name="My Thoughts" icon="💭" label="writing" />
             <CardContainer minWidth="300px">
               {posts.map(({ Component, ...rest }) => (
                 <Fade bottom key={rest.id}>
