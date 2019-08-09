7736953ed62ee8d5e59d3702e6c5d597d72ebe6f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-08 23:23:46 -0400

    Changes width of portfolio-logo

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -51,7 +51,7 @@ const Header = () => (
             const homeLink = home && (
               <Image
                 src={Logo}
-                width="50px"
+                width="60px"
                 alt="Portfolio Logo"
                 onClick={home.onClick}
                 style={{
