e385046eba416226757682f7a44b871856d34a2f
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-08-10 15:12:20 -0400

    Fixes margin-top of about-me section

--- portfolio/src/components/Section.js
+++ portfolio/src/components/Section.js
@@ -15,7 +15,7 @@ const SectionContainer = styled.div`
   flex: 0 1 auto;
   flex-direction: column;
   justify-content: center;
-  padding: 5em 1em;
+  padding: 1em 1em;
   scroll-behavior: smooth;
 `;
 
