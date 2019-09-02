59b755fb44f613cd8bddfaa14f24e9a342078bbf
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-09-02 00:50:34 -0400

    Updates title in web browser tab

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -30,7 +30,7 @@ const Helmet = ({ theme = {} }) => (
     `}
     render={data => {
       const { name, description, profile } = data.contentfulAbout;
-      const title = `${name}`;
+      const title = `${name} 'tep-sir-in-tawn'`;
 
       return (
         <ReactHelmet htmlAttributes={{ lang: 'en' }}>
